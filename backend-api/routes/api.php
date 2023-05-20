1e90d445eed12a5629eef95ba587abd9e0b985d7
RodSoriano <rsoriano.jose@gmail.com>
2023-05-19 20:23:28 -0600

    Create route api for payment

--- backend-api/routes/api.php
+++ backend-api/routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::post('payment', [PaymentController::class, 'create']);
 });
